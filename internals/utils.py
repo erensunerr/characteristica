48dbb95b18e7ae140a06eff959febc013ad021df
Eren Suner <>
2021-04-27 17:30:05 -0400

    some updates

--- internals/utils.py
+++ internals/utils.py
@@ -3,4 +3,4 @@ import logging
 logger = logging.getLogger('logger')
 FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(format=FORMAT)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.WARNING)
--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from language.characteristicaLexer import characteristicaLexer
 from language.characteristicaParser import characteristicaParser
 from visitor import CustomVisitor
 from antlr4 import FileStream, CommonTokenStream
-import sys
+import sys, tabulate
 from internals.proofVerify import *
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
+        print("\n")
         cv = CustomVisitor()
         fst = FileStream(sys.argv[1])
         lex = characteristicaLexer(fst)
@@ -14,10 +15,16 @@ if __name__ == '__main__':
         parser = characteristicaParser(tokenst)
         tree = parser.prog()
         cv.visit(tree)
+        print("\n")
+        truthList = []
+        for stat in cv.statDict.values():
+            truthList += [[stat.name, stat.isVerified]]
+        print(tabulate.tabulate(truthList, headers=["Name", "Verified"]))
         tcAxList = tcList_to_tcAxList(cv.tcList)
         verify(tcAxList)
+        truthList = []
+        print("\n")
         for stat in cv.statDict.values():
-            print(f"{stat} {stat.isVerified}")
-
-    
-
+            truthList += [[stat.name, stat.isVerified]]
+        print(tabulate.tabulate(truthList, headers=["Name", "Verified"]))
+        print("\n")
